58ad3ec49841a3c4969c7f4c520ff0940da777fa
Raj9457 <rajatbisht508@gmail.com>
2023-11-28 11:42:44 +0530

    promises

--- Java_Scripts/day5(Promises)/index.js
+++ Java_Scripts/day5(Promises)/index.js
@@ -1,1 +1,17 @@
-  
+  // ----Promises -----
+  let url1="my name is rajat"
+  function download_url(url){
+    return new Promise(function(resolve,rejecct){
+        console.log("inside promise");
+        resolve(url);
+    })
+  }
+
+  function download_it(data){
+    console.log(data);
+  }
+download_url(url1).then(function(res){
+    download_it(res);
+}).catch(function(error){
+    console.log(error);
+})
